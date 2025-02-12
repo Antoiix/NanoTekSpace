c5854e0e624d8fc775dc519c286f84f3b2e7679b
ktimote <timote.koenig@epitech.eu>
2025-02-12 17:20:46 +0100

    feat(parsing): my_str_to_word_array add function to .hpp

--- include/str_to_word_array.hpp
+++ include/str_to_word_array.hpp
@@ -9,4 +9,6 @@
 #include <list>
 #include <string>
 
+std::list<std::string> my_str_to_word_array(const std::string &buffer, const std::string &delimiter);
+
 #endif //NANOTEKSPICE_STR_TO_WORD_ARRAY_HPP
