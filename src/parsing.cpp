b7af3549abea0cae99afce42eff424862b88dec1
ktimote <timote.koenig@epitech.eu>
2025-02-12 14:49:00 +0100

    feat(parsing): my_str_to_word_array can take multiple delimiter

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -5,24 +5,50 @@
 ** parsing.cpp
 */
 
-#include "parsing.hpp"
+#include "../include/parsing.hpp"
 
-std::list<std::string> my_str_to_word_array(const std::string& buffer, const std::string& delimiter)
+//std::list<std::string> my_str_to_word_array(const std::string& buffer, const std::string& delimiter)
+//{
+//    std::list<std::string> word_array;
+//    std::string temp;
+//    size_t buffer_len = buffer.length();
+//    size_t find_indice = 0;
+//    size_t indice = 0;
+//
+//    find_indice = buffer.find(delimiter, indice);
+//    while (find_indice != std::string::npos){
+//        if (indice != find_indice)
+//            word_array.push_back(buffer.substr(indice));
+//        indice = find_indice + 1;
+//        find_indice = buffer.find(delimiter, indice);
+//    }
+//    if (indice != buffer_len)
+//        word_array.push_back(buffer.substr(indice));
+//    return word_array;
+//}
+
+bool is_delimiter(char c, const std::string &delimiter)
+{
+    return (delimiter.find(c) != std::string::npos);
+}
+
+std::list<std::string> my_str_to_word_array(const std::string &buffer, const std::string &delimiter)
 {
     std::list<std::string> word_array;
-    std::string temp;
-    size_t buffer_len = buffer.length();
-    size_t find_indice = 0;
-    size_t indice = 0;
+    std::string word = "";
 
-    find_indice = buffer.find(delimiter, indice);
-    while (find_indice != std::string::npos){
-        if (indice != find_indice)
-            word_array.push_back(buffer.substr(indice));
-        indice = find_indice + 1;
-        find_indice = buffer.find(delimiter, indice);
+    for (char c: buffer) {
+        if (is_delimiter(c, delimiter)) {
+            if (!word.empty()) {
+                std::cout << word << std::endl;
+                word_array.push_back(word);
+                word = "";
+            }
+        } else
+            word += c;
     }
-    if (indice != buffer_len)
-        word_array.push_back(buffer.substr(indice));
+    if (!word.empty())
+        word_array.push_back(word);
+
     return word_array;
 }
