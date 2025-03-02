7113d268fbb04839f81491dc2fb4601b16a4e485
ktimote <timote.koenig@epitech.eu>
2025-03-02 12:03:24 +0100

    feat(shell): loop shell function

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -50,10 +50,11 @@ void Shell::getExecutionCommands()
     }
     this->setOutputString(firstSs.str());
 
+    std::cout << "> ";
     while (std::getline(std::cin, buffer) && buffer != "exit") {
-        std::cout << "> ";
         if (buffer == "display") {
             std::cout << this->getOutputString();
+            std::cout << "> ";
             continue;
         }
         if (buffer == "simulate") {
@@ -77,23 +78,45 @@ void Shell::getExecutionCommands()
                 ss << "\t" << currentOutput << ": " << tmpOutput->compute(1, this->components_map) << std::endl;
             }
             this->setOutputString(ss.str());
+            std::cout << "> ";
             continue;
         }
         if (buffer == "loop") {
-            std::cout << "loop" << std::endl;
             std::signal(SIGINT, signalHandler);
             Utils::loopExitFlag = true;
             while (Utils::loopExitFlag){
-                // simulate function
+                std::stringstream ss;
+                this->components_map.incrementTick();
+                ss << "tick: " << this->components_map.getTick() << std::endl;
+                ss << "input(s):" << std::endl;
+                for (const auto& currentInput : this->listInputs)
+                {
+                    auto tmpInput = this->components_map.getComponent(currentInput);
+                    if (tmpInput == nullptr)
+                        continue;
+                    ss << "\t" << currentInput << ": " << tmpInput->compute(1, this->components_map) << std::endl;
+                }
+                ss << "output(s):" << std::endl;
+                for (const auto& currentOutput : this->listOutputs)
+                {
+                    auto tmpOutput = this->components_map.getComponent(currentOutput);
+                    if (tmpOutput == nullptr)
+                        continue;
+                    ss << "\t" << currentOutput << ": " << tmpOutput->compute(1, this->components_map) << std::endl;
+                }
+                this->setOutputString(ss.str());
             }
-            std::cout << "display" << std::endl;
+            std::cout << this->getOutputString();
+            std::cout << "> ";
             continue;
         }
         if (inputIsValid(buffer)) {
             std::cout << "change input" << std::endl;
+            std::cout << "> ";
             continue;
         }
         std::cout << "Invalid Command" << std::endl;
+        std::cout << "> ";
     }
 }
 
