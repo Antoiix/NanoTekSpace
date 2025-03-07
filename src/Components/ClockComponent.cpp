831a294ef8395418740817e3b604075ef1077085
ktimote <timote.koenig@epitech.eu>
2025-03-07 15:58:52 +0100

    fix(clock): inverted clock

--- src/Components/ClockComponent.cpp
+++ src/Components/ClockComponent.cpp
@@ -17,17 +17,25 @@ nts::ClockComponent::ClockComponent(std::string name) : AComponent(1, std::move(
 nts::Tristate nts::ClockComponent::compute(std::size_t pin, Map &map)
 {
     (void)map;
+    auto tmpPin = _pins.at(pin);
+    if (tmpPin != nullptr)
+        return tmpPin->getState();
+    return Tristate::Undefined;
+}
+
+nts::Tristate nts::ClockComponent::getPinState(std::size_t pin)
+{
     (void)pin;
 
     switch (this->_pins[1]->getState()) {
-      case Undefined:
-        return Undefined;
-      case True:
-        this->_pins[1]->setState(False);
-        return True;
-      case False:
-        this->_pins[1]->setState(True);
-        return False;
+        case Undefined:
+            return Undefined;
+        case True:
+            this->_pins[1]->setState(False);
+        return this->_pins[1]->getState();
+        case False:
+            this->_pins[1]->setState(True);
+        return this->_pins[1]->getState();
     }
     return Undefined;
 }
@@ -36,6 +44,16 @@ void nts::ClockComponent::changePinState(std::size_t pin, Tristate newState)
 {
     auto tmpPin = this->_pins.at(pin);
     if (tmpPin != nullptr)
-        tmpPin->setState(newState);
+        switch (newState) {
+            case True:
+              tmpPin->setState(False);
+              break;
+            case False:
+              tmpPin->setState(True);
+              break;
+            default:
+              tmpPin->setState(Undefined);
+              break;
+        }
 }
 
